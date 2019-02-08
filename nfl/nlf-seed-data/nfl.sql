794563d4e70b5e1330175327a1f6861737c3b6e7
destin floyd <destin.b.floyd@gmail.com>
2019-02-07 22:08:53 -0500

    Commit: NFL - wow there's a lot of nfl data

--- nfl/nlf-seed-data/nfl.sql
+++ nfl/nlf-seed-data/nfl.sql
@@ -6,4 +6,11 @@ select stadium, head_coach  from teams;
  count
 -------
   1532
-  
+  select head_coach  from teams where conference = 'AFC' and division = 'North' or division = 'East' ;
+  select name, salary  from players order by salary desc;
+  select AVG(salary) from players;
+         avg
+----------------------
+ 1579692.539817232376
+
+  select name, position from players where salary > 10000000;
