dd5332d9860fe5955a2bf69ebe0bcffe48c880a9
destin floyd <destin.b.floyd@gmail.com>
2019-02-07 22:43:32 -0500

    Commit: NFL - Finished the nfl queries

--- nfl/nlf-seed-data/nfl.sql
+++ nfl/nlf-seed-data/nfl.sql
@@ -14,3 +14,37 @@ select stadium, head_coach  from teams;
  1579692.539817232376
 
   select name, position from players where salary > 10000000;
+
+  select max (salary) from players ;
+  nfl=# select name from players where salary = 18000000;
+      name
+----------------
+ Peyton Manning
+
+select name, position from players order by salary desc limit 100;
+
+select avg(salary) from players where position='DE';
+         avg
+----------------------
+ 2161326.377049180328
+
+ select * from teams;
+--  / id buffalo bills = 1
+ select players.name from players  where team_id = '1';
+
+ select * from teams;
+--  / id NY Giants = 18
+ nfl=# select SUM(players.salary) from players where team_id = '18';
+   sum
+----------
+ 74179466
+
+select * from teams;
+--  / id greenbay packers = 23
+ select salary  from players where players.team_id = '23' order by salary Asc;
+ 390000
+
+ select salary, name  from players where players.team_id = '23' and salary = 390000;
+ salary |      name
+--------+----------------
+ 390000 | Shaky Smithson
